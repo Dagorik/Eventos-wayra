e5fb457836ee834d9f635e635cde872c4c701185
NT - Ernesto Ramirez <NearTox@outlook.com>
2016-11-29 20:53:20 -0600

    fix

--- eventos/settings.py
+++ eventos/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'r%q!t4rqhxn&7n6m11qsh%lu#(%v0ssffne!j27dmk^e4drlnr'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
